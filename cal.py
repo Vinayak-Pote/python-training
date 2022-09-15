6313258a196f33d744252d1e8a015ad2a4f1ee35
Vinayak <pote_vinayak@lilly.com>
2022-09-15 12:37:45 +0530

    Adding Module

---
+++ cal.py
@@ -0,0 +1,8 @@
+def hra(bs):
+    return bs *0.5
+
+def da(bs):
+    return bs * .3
+
+def bonus(bs):
+    return bs * .1
---
+++ main.py
@@ -0,0 +1,6 @@
+import cal
+
+bs = int(input("Enter basic salary: "))
+print("HRA: ",cal.hra(bs))
+print("DA: ",cal.da(bs))
+print("Bonus: ",cal.bonus(bs))
